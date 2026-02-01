84658058880b6366609841e67350ac331b1c931e
Ezeki Zibzibadze <ez@jok.io>
2026-02-01 23:42:06 +0400

    feat: add create api and allow buy/sell only self created erc20 tokens

--- src/TokenLauncher.sol
+++ src/TokenLauncher.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { IHooks } from "v4-core/interfaces/IHooks.sol";
-import { BondingCurveLib } from "./helpers/BondingCurveLib.sol";
-import { IPositionManager } from "v4-periphery/src/interfaces/IPositionManager.sol";
-import { UniswapV4Lib } from "./helpers/UniswapV4Lib.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import {BondingCurveLib} from "./helpers/BondingCurveLib.sol";
+import {
+    IPositionManager
+} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {UniswapV4Lib} from "./helpers/UniswapV4Lib.sol";
+import {
+    ReentrancyGuard
+} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {BondingToken} from "./BondingToken.sol";
 
 interface IMintBurnERC20 {
     function mint(address, uint256) external;
@@ -17,25 +22,55 @@ interface IMintBurnERC20 {
 }
 
 contract TokenLauncher is ReentrancyGuard {
-    event Graduated(address indexed token, uint256 ethAmount, uint256 tokenId);
+    enum TokenStatus {
+        None, // 0 - default/uninitialized
+        Registered, // 1
+        Graduated // 2
+    }
+
+    event TokenCreated(address indexed token);
+    event TokenGraduated(
+        address indexed token,
+        uint256 ethAmount,
+        uint256 tokenId
+    );
 
     uint24 public constant LP_FEE = 3000; // 0.3%
     int24 public constant TICK_SPACING = 60;
 
     IPositionManager public positionManager;
 
-    mapping(address => bool) public graduated;
+    mapping(address => TokenStatus) public tokenStatus;
     mapping(address => uint256) public ethReserves;
 
     constructor(address positionManagerAddr) {
         positionManager = IPositionManager(positionManagerAddr);
     }
 
-    receive() external payable { }
+    receive() external payable {}
 
     // public api
-    function buy(address tokenAddr, uint256 amt, uint256 maxEthAmt) public payable nonReentrant returns (uint256) {
-        require(!graduated[tokenAddr], "Token already graduation, please use LP for swaps");
+    function create(
+        string calldata name,
+        string calldata symbol
+    ) external returns (address tokenAddr) {
+        BondingToken token = new BondingToken(name, symbol, address(this));
+        tokenAddr = address(token);
+
+        tokenStatus[tokenAddr] = TokenStatus.Registered;
+
+        emit TokenCreated(tokenAddr);
+    }
+
+    function buy(
+        address tokenAddr,
+        uint256 amt,
+        uint256 maxEthAmt
+    ) public payable nonReentrant returns (uint256) {
+        require(
+            tokenStatus[tokenAddr] == TokenStatus.Registered,
+            "Token not registered or already graduated"
+        );
 
         IMintBurnERC20 token = IMintBurnERC20(tokenAddr);
         address user = msg.sender;
@@ -54,7 +89,7 @@ contract TokenLauncher is ReentrancyGuard {
 
         uint256 ethToRefund = msg.value - ethToCharge;
         if (ethToRefund > 0) {
-            (bool success,) = user.call{ value: ethToRefund }("");
+            (bool success, ) = user.call{value: ethToRefund}("");
             require(success, "ETH refund failed");
         }
 
@@ -65,8 +100,15 @@ contract TokenLauncher is ReentrancyGuard {
         return ethToCharge;
     }
 
-    function sell(address tokenAddr, uint256 amt, uint256 minEthAmount) public nonReentrant returns (uint256) {
-        require(!graduated[tokenAddr], "Token already graduation, please use LP for swaps");
+    function sell(
+        address tokenAddr,
+        uint256 amt,
+        uint256 minEthAmount
+    ) public nonReentrant returns (uint256) {
+        require(
+            tokenStatus[tokenAddr] == TokenStatus.Registered,
+            "Token not registered or already graduated"
+        );
 
         IMintBurnERC20 token = IMintBurnERC20(tokenAddr);
         address user = msg.sender;
@@ -81,7 +123,7 @@ contract TokenLauncher is ReentrancyGuard {
 
         ethReserves[tokenAddr] -= refund;
 
-        (bool success,) = user.call{ value: refund }("");
+        (bool success, ) = user.call{value: refund}("");
         require(success, "ETH transfer failed");
 
         return refund;
@@ -91,7 +133,7 @@ contract TokenLauncher is ReentrancyGuard {
     function _graduate(address tokenAddr) internal returns (uint256 ethAmount) {
         IMintBurnERC20 token = IMintBurnERC20(tokenAddr);
 
-        graduated[tokenAddr] = true;
+        tokenStatus[tokenAddr] = TokenStatus.Graduated;
 
         ethAmount = ethReserves[tokenAddr];
         ethReserves[tokenAddr] = 0;
@@ -100,8 +142,11 @@ contract TokenLauncher is ReentrancyGuard {
         // At graduation: virtual token reserve = 600M, virtual ETH reserve = 40 ETH
         // Price = 40 ETH / 600M tokens
         // For the ETH we have, mint proportional tokens
-        uint256 tokensForLp = (ethAmount * BondingCurveLib.VIRTUAL_TOKEN_RESERVE)
-            / (BondingCurveLib.K / (BondingCurveLib.VIRTUAL_TOKEN_RESERVE - BondingCurveLib.SUPPLY_CAP));
+        uint256 tokensForLp = (ethAmount *
+            BondingCurveLib.VIRTUAL_TOKEN_RESERVE) /
+            (BondingCurveLib.K /
+                (BondingCurveLib.VIRTUAL_TOKEN_RESERVE -
+                    BondingCurveLib.SUPPLY_CAP));
 
         // Mint tokens for LP
         token.mint(address(this), tokensForLp);
@@ -126,6 +171,6 @@ contract TokenLauncher is ReentrancyGuard {
         // Renounce admin after LP creation
         token.renounceOwnership();
 
-        emit Graduated(tokenAddr, ethAmount, tokenId);
+        emit TokenGraduated(tokenAddr, ethAmount, tokenId);
     }
 }
